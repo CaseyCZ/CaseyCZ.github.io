fb23cf3e92723335d83558c2a89de47ec3406b26
Lukáš <kyselalukas@seznam.cz>
2023-09-01 13:36:13 +0200

    mobil

--- script.js
+++ script.js
@@ -7,28 +7,12 @@ document.addEventListener('DOMContentLoaded', function () {
   const nightModeButton = document.getElementById('nightModeButton');
   nightModeButton.addEventListener('click', toggleNightMode);
 });
-
-// Funkce pro zobrazení/skrytí hamburger menu
+// Funkce pro otevření/zavření menu
 function toggleMenu() {
   const navLinks = document.querySelector('.nav-links');
   navLinks.classList.toggle('show-menu');
 }
 
-// Získání odkazu na hamburger menu
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-
-// Získání odkazu na navigační menu
-const navLinks = document.querySelector('.nav-links');
-
-// Přidání posluchače události pro kliknutí na hamburger menu
-hamburgerMenu.addEventListener('click', () => {
-  toggleMenu();
-});
-
-// Přidání posluchače události pro zavření menu po kliknutí na odkaz
-const navLinksArray = navLinks.querySelectorAll('.nav-link');
-navLinksArray.forEach((link) => {
-  link.addEventListener('click', () => {
-    toggleMenu();
-  });
-});
+// Přidání posluchače události pro zobrazení/skrytí menu po kliknutí na ikonu hamburger menu
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+hamburgerIcon.addEventListener('click', toggleMenu);

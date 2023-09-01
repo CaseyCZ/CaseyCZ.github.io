df88a28d32e25d08e08f63705037cac5007cd6e0
Lukáš <kyselalukas@seznam.cz>
2023-09-01 13:12:01 +0200

    cely kod

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-// script.js
+// Funkce pro přepínání nočního režimu
 function toggleNightMode() {
-    document.body.classList.toggle('night-mode');
-  }
-  
-  document.addEventListener('DOMContentLoaded', function () {
-    const nightModeButton = document.getElementById('nightModeButton');
-    nightModeButton.addEventListener('click', toggleNightMode);
-  });
-  // Získejte odkaz na hamburger menu
-const hamburgerMenu = document.querySelector('.hamburger-menu');
+  document.body.classList.toggle('night-mode');
+}
 
-// Získejte odkaz na navigační menu
-const navLinks = document.querySelector('.nav-links');
-
-// Přidejte posluchače události pro kliknutí na hamburger menu
-hamburgerMenu.addEventListener('click', () => {
-  // Přepněte třídu 'show-menu' pro navigační menu
-  navLinks.classList.toggle('show-menu');
+document.addEventListener('DOMContentLoaded', function () {
+  const nightModeButton = document.getElementById('nightModeButton');
+  nightModeButton.addEventListener('click', toggleNightMode);
 });
 
-// Přidejte posluchače události pro zavření menu po kliknutí na odkaz (volitelné)
+// Funkce pro zobrazení/skrytí hamburger menu
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('show-menu');
+}
+
+// Získání odkazu na hamburger menu
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+// Získání odkazu na navigační menu
+const navLinks = document.querySelector('.nav-links');
+
+// Přidání posluchače události pro kliknutí na hamburger menu
+hamburgerMenu.addEventListener('click', () => {
+  toggleMenu();
+});
+
+// Přidání posluchače události pro zavření menu po kliknutí na odkaz
 const navLinksArray = navLinks.querySelectorAll('.nav-link');
 navLinksArray.forEach((link) => {
   link.addEventListener('click', () => {
-    // Odeberte třídu 'show-menu' z navigačního menu po kliknutí na odkaz
-    navLinks.classList.remove('show-menu');
+    toggleMenu();
   });
 });
-function toggleMenu() {
-  var menu = document.querySelector('.hamburger-menu');
-  menu.classList.toggle('show-menu');
-}

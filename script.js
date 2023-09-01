3c37394ca9cc1596a859f98ed68c038df77fce22
Lukáš <kyselalukas@seznam.cz>
2023-09-01 12:12:15 +0200

    hamburger

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ function toggleNightMode() {
     const nightModeButton = document.getElementById('nightModeButton');
     nightModeButton.addEventListener('click', toggleNightMode);
   });
+  // Získejte odkaz na hamburger menu
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+// Získejte odkaz na navigační menu
+const navLinks = document.querySelector('.nav-links');
+
+// Přidejte posluchače události pro kliknutí na hamburger menu
+hamburgerMenu.addEventListener('click', () => {
+  // Přepněte třídu 'show-menu' pro navigační menu
+  navLinks.classList.toggle('show-menu');
+});
+
+// Přidejte posluchače události pro zavření menu po kliknutí na odkaz (volitelné)
+const navLinksArray = navLinks.querySelectorAll('.nav-link');
+navLinksArray.forEach((link) => {
+  link.addEventListener('click', () => {
+    // Odeberte třídu 'show-menu' z navigačního menu po kliknutí na odkaz
+    navLinks.classList.remove('show-menu');
+  });
+});
